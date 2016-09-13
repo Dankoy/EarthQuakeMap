ecc725440e69200d69be73c9d40821b49709767a
Evgeny Zelenin <transingularity@gmail.com>
2016-09-13 16:37:45 +0300

    Added comments

--- EarthQuakeMap/src/quakemap/TwoMaps.java
+++ EarthQuakeMap/src/quakemap/TwoMaps.java
@@ -12,17 +12,22 @@ public class TwoMaps extends PApplet {
 
 	private static final long serialVersionUID = 1L;
 	
+	// Variable is using to find out if you are working with or without Internet connection
 	private static final boolean offline = false;
 	
+	// This is where to find the local tiles, for working without an Internet connection
 	public static String mbTilesString = "blankLight-1-3.mbtiles";
 	
 	UnfoldingMap map1;
 	
 	public void setup() {
+		
+		// This sets the background color for the Applet.  
 		size(800, 600);
+		
 		this.background(200, 200, 200);
 		
-		
+		// Provider for map
 		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
 		int zoomLevel = 10;
 		
@@ -31,8 +36,10 @@ public class TwoMaps extends PApplet {
 			zoomLevel = 3;
 		}
 		
-		map1 = new UnfoldingMap(this, 100, 50, 50, 50, provider);
+		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
 		map1.zoomAndPanTo(zoomLevel, new Location(47.441812f, 7.7644f));
+		
+		// Makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map1);
 		
 	}
